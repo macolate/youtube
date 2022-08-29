b958952fcd1023038004de5ab96041a9417e55cd
macolate <jisok13@gmail.com>
2022-08-29 17:54:53 +0900

    검색기능 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import Styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    youtube
+      .search(query)
+      .then(videos => setVideos(videos));
+  };
+  
+  useEffect(() => {
+    youtube
+      .mostPopular()
+      .then(videos => setVideos(videos));
 
-  useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCrzATepCsPYpVf46rSZTWPZP5gENG-ZB0",
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error))
   }, []);
+
   return (
-    <VideoList videos={videos} />
+    <div className={Styles.app}>
+      <SearchHeader onSearch={search}/>
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
